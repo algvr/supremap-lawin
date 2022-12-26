058b0c4bd4460de5518edf25240a63467c6f579b
Alexey Gavryushin <a.gavryushin@tum.de>
2022-12-26 21:18:50 +0100

    Set reduce_zero_label=False for DummyDataset

--- mmseg/datasets/dummy.py
+++ mmseg/datasets/dummy.py
@@ -7,11 +7,11 @@ class DummyDataset(CustomDataset):
     CLASSES = ('background', 'building', 'water', 'green', 'highway', 'beach', 'road')
 
     PALETTE = [[255, 255, 255], [128, 128, 128], [0, 0, 128], [128, 0, 0], [255, 110, 0],
-               [255, 216, 0], [64, 64, 64]]
+               [255, 216, 0], [0, 0, 0]]
 
     def __init__(self, **kwargs):
         super(DummyDataset, self).__init__(
             img_suffix='.jpg',
             seg_map_suffix='.png',
-            reduce_zero_label=True,
+            reduce_zero_label=False,
             **kwargs)
